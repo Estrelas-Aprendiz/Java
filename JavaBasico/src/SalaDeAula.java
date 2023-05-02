dafe3c4453083d415904f6f5823a12ee4424a69b
Carolina De Carvalho Queiroz <88510089+carolina-queiroz@users.noreply.github.com>
2023-05-02 11:31:48 -0300

    Exemplo de Static

---
+++ JavaBasico/src/SalaDeAula.java
@@ -0,0 +1,11 @@
+public class SalaDeAula {
+    static String aluno;
+    static String professor;
+
+    static void ensinar(){
+        System.out.println(professor+ " Ensinando");
+//        System.out.println(aluno);
+
+    }
+
+}
